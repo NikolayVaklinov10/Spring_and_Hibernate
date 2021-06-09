95c0df0b24808dfcb29dd9f16a0bc587deb7302d
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-06-09 08:36:46 +0100

    pojo version of the application

---
+++ src/main/java/com/nikolayvaklinov/SpringDemo/MyApp.java
@@ -0,0 +1,15 @@
+package com.nikolayvaklinov.SpringDemo;
+
+public class MyApp {
+
+    public static void main(String[] args) {
+
+        // create the object
+        // Coach theCoach = new BaseballCoach():
+        Coach theCoach = new BaseballCoach();
+
+        // use the object
+        System.out.println(theCoach.getDailyWorkout());
+
+    }
+}
