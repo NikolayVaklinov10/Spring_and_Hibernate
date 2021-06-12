986405a23f8a9f276c8cbfdcd23e3828f0d9a5e9
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-06-12 16:28:12 +0100

    reading configuration directly from a java class

---
+++ src/main/java/com/nikolayvaklinov/SpringAnotations/SportConfig.java
@@ -0,0 +1,9 @@
+package com.nikolayvaklinov.SpringAnotations;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.nikolayvaklinov.SpringAnotations")
+public class SportConfig {
+}
