9eaab686b97c203e753912f1bdab140907232628
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-06-13 21:50:28 +0100

    the third version of the form

--- SpringMVC/src/main/java/com/nikolayvaklinov/springMVC/HelloWorldController.java
+++ SpringMVC/src/main/java/com/nikolayvaklinov/springMVC/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.nikolayvaklinov.springMVC;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -39,5 +40,25 @@ public class HelloWorldController {
 
         return "helloworld";
     }
+
+
+
+    // new controller method to read form data and add data to the model
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Hey My Friend from v3!" + theName;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+
 
 }
