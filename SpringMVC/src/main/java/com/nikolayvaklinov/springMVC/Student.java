df4800b7198596c4e4733605781942d7b28ab521
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-06-13 23:50:29 +0100

    the Student class

---
+++ SpringMVC/src/main/java/com/nikolayvaklinov/springMVC/Student.java
@@ -0,0 +1,27 @@
+package com.nikolayvaklinov.springMVC;
+
+public class Student {
+
+    private String firstName;
+    private String lastName;
+
+    // an empty default constructor
+    public Student() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
