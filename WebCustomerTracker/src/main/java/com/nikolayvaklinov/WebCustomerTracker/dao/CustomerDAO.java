39c7236114bc1e40f069777d4fd8c30981cacd1e
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-06-18 23:02:21 +0100

    note: but needs to be fixed

---
+++ WebCustomerTracker/src/main/java/com/nikolayvaklinov/WebCustomerTracker/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.nikolayvaklinov.WebCustomerTracker.dao;
+
+import com.nikolayvaklinov.WebCustomerTracker.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerDAO {
+
+    public List<Customer> getCustomer();
+}
