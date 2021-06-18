ac8652a9d32731e46d7b37a7013091848f179ffd
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-06-18 22:58:53 +0100

    note: but needs to be fixed

---
+++ WebCustomerTracker/src/main/java/com/mchange/v2/c3p0/ComboPooledDataSource.java
@@ -0,0 +1,27 @@
+package com.mchange.v2.c3p0;
+
+public class ComboPooledDataSource {
+    private void close() {
+    }
+
+    public void setDriverClass(String driverClass) {
+    }
+
+    public void setMaxIdleTime(String maxIdleTime) {
+    }
+
+    public void setMinPoolSize(String minPoolSize) {
+    }
+
+    public void setUser(String user) {
+    }
+
+    public void setJdbcUrl(String jdbcUrl) {
+    }
+
+    public void setPassword(String password) {
+    }
+
+    public void setMaxPoolSize(String maxPoolSize) {
+    }
+}
