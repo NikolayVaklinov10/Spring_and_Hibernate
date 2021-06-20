3a0dcc700736e490028d8f1d04635967b9407146
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-06-20 15:45:34 +0100

    another DAO class

---
+++ SpringAOP/src/main/java/com/nikolayvaklinov/aopdemo/dao/MembershipDAO.java
@@ -0,0 +1,11 @@
+package com.nikolayvaklinov.aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MembershipDAO {
+
+    public void addAccount() {
+        System.out.println(getClass() +": DOING STUFF: ADDING MEMBERSHIP ACCOUNT");
+    }
+}
